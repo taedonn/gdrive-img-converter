2caa16a09448ed1b844582304629c7b0b4a7ef5f
taedonn <taedonn@outlook.com>
2023-03-14 14:52:27 +0900

    Add copy button, image preview

--- js/settings.js
+++ js/settings.js
@@ -4,3 +4,35 @@ function radioOnChange(thisRadio) {
     for(let i = 0; i < radioLink.length; i++) { radioLink[i].style.display = 'none'; }
     document.getElementsByClassName(radioId)[0].style.display = 'block';
 }
+
+function linkOriginClick() {
+    let linkOrigin = document.getElementById('link_origin').value;
+    let linkStart = linkOrigin.indexOf('/d/');
+    let linkEnd = linkOrigin.indexOf('/view');
+    let linkId = linkOrigin.substring(linkStart + 3,linkEnd);
+
+    document.getElementById('input_converted_1').value = 'https://drive.google.com/uc?id=' + linkId;
+    document.getElementById('input_converted_2').value = '<img src="https://drive.google.com/uc?id=' + linkId + '" alt="Google Drive Image"/>';
+    document.getElementById('input_converted_3').value = '<a href="https://drive.google.com/uc?id=' + linkId + '"><img src="https://drive.google.com/uc?id=' + linkId + '" alt="Google Drive Image"/>';
+    document.getElementsByClassName('link_img_wrap')[0].innerHTML = '<img src="https://drive.google.com/uc?id=' + linkId + '" alt="Google Drive Image"/>';
+}
+
+function linkOriginEnter() {
+	if (window.event.keyCode == 13) { // 엔터키 눌렀을 때
+        linkOriginClick();
+    }
+}
+
+function copyOnClick(thisBtn) {
+    let copyBtn = thisBtn.getAttribute('class');
+    let copyBtnId = copyBtn.substring(copyBtn.lastIndexOf('copy') + 4, copyBtn.lastIndexOf('_') + 2);
+
+    window.navigator.clipboard.writeText(document.getElementById('input_converted' + copyBtnId).value);
+
+    document.getElementsByClassName('link_converted' + copyBtnId)[0].getElementsByClassName('copy_btn')[0].style.display = 'none';
+    document.getElementsByClassName('link_converted' + copyBtnId)[0].getElementsByClassName('copy_check_btn')[0].style.display = 'block';
+    setTimeout(function() {
+        document.getElementsByClassName('link_converted' + copyBtnId)[0].getElementsByClassName('copy_check_btn')[0].style.display = 'none';
+        document.getElementsByClassName('link_converted' + copyBtnId)[0].getElementsByClassName('copy_btn')[0].style.display = 'block';
+    },1500);
+}
